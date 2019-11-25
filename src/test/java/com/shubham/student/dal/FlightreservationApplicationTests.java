bb8fea00cc58e6492680c45f67bae17a1a82d3f3
Shubham Yadav <shubhamyadav03.2011@gmail.com>
2019-11-25 11:51:29 +0530

    First commit

---
+++ src/test/java/com/shubham/student/dal/FlightreservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.student.dal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightreservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
