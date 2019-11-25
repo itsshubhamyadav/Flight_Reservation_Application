772f4b4907c2e8456bc42506dd458b8b3c678a86
Shubham Yadav <shubhamyadav03.2011@gmail.com>
2019-11-25 23:23:48 +0530

    added entity class i.e. user,passenger,flight,reservation

--- src/main/java/com/shubham/flightreservation/FlightreservationApplication.java
+++ src/main/java/com/shubham/flightreservation/FlightreservationApplication.java
@@ -1,4 +1,4 @@
-package com.shubham.student.dal;
+package com.shubham.flightreservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
