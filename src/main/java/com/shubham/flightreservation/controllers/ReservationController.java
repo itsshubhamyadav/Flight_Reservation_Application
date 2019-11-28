66ac0611a147bff7addc9d47a5297e7ef7f005e8
Shubham Yadav <shubhamyadav03.2011@gmail.com>
2019-11-28 12:24:51 +0530

    created reservation confirmation view with msg

--- src/main/java/com/shubham/flightreservation/controllers/ReservationController.java
+++ src/main/java/com/shubham/flightreservation/controllers/ReservationController.java
@@ -6,17 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.shubham.flightreservation.dto.ReservationRequest;
 import com.shubham.flightreservation.entities.Flight;
+import com.shubham.flightreservation.entities.Reservation;
 import com.shubham.flightreservation.repos.FlightRepository;
+import com.shubham.flightreservation.services.ReservationService;
 
 @Controller
 public class ReservationController {
 	
 	@Autowired
 	FlightRepository flightRepository; 
+	
+	@Autowired
+	ReservationService reservationService;
 	
 	@RequestMapping("/showCompleteReservation")
 	public String showCompleteReservation(@RequestParam("flightId") Long flightId, ModelMap modelMap) {
@@ -25,9 +31,14 @@ public class ReservationController {
 		return "completeReservation";
 		
 	}
-	@RequestMapping("/showCompleteReservation")
-	public String completeReservation(ReservationRequest reservationRequest) {
-		return null;
+	@RequestMapping(value="/completeReservation",method=RequestMethod.POST)
+	public String completeReservation(ReservationRequest request,ModelMap modeMap) {
+		
+		Reservation reservation=reservationService.bookFlight(request);
+		modeMap.addAttribute("msg","Reservation"
+				+ " created successfully and the id is "+reservation.getId());
+		
+		return "reservationConfirmation";
 		
 	}
 
