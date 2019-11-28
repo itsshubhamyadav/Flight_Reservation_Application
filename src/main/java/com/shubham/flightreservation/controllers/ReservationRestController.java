3ad5e74a421b3c152c5812c09948f7796a93df79
Shubham Yadav <shubhamyadav03.2011@gmail.com>
2019-11-28 13:59:56 +0530

    updated the reservation controller

--- src/main/java/com/shubham/flightreservation/controllers/ReservationRestController.java
+++ src/main/java/com/shubham/flightreservation/controllers/ReservationRestController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.shubham.flightreservation.dto.ReservationUpdateRequest;
 import com.shubham.flightreservation.entities.Reservation;
 import com.shubham.flightreservation.repos.ReservationRepository;
 
@@ -17,5 +18,14 @@ public class ReservationRestController {
 	@RequestMapping("/reservation/{id}")
 	public Reservation findReservation(@PathVariable("id") Long id) {
 		return reservationRepository.findById(id).get();
+	}
+	
+	@RequestMapping("/reservation/{id}")
+	public Reservation updateReservation(ReservationUpdateRequest request) {
+		Reservation reservation = reservationRepository.findById(request.getId()).get();
+		reservation.setNumberOfBags(request.getNumberOfBags());
+		reservation.setCheckedIn(request.getCheckedIn());
+		Reservation updatedReservation = reservationRepository.save(reservation);
+		return updatedReservation;
 	}
 }
